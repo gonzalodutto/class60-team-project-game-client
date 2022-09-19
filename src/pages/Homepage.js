881674cd99c54647e8c3b3b32113c7daa9ac89bf
JS1209 <J.a.schoenmakers@outlook.com>
2022-09-19 13:29:12 +0200

    PointToPointCursor game finished. Solution: make window smaller and use off-window space to get to the end

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,26 +1,28 @@
-import { Title } from "../styled"
-import { Link } from "react-router-dom"
-import { LinkWord } from "../styled"
-import styled from "styled-components"
+import { Title } from "../styled";
+import { Link } from "react-router-dom";
+import { LinkWord } from "../styled";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
 
 export const Homepage = () => {
-
+  const navigate = useNavigate();
   return (
     <Container>
-     <h3>Hello there 👋</h3>
-     <p>General information:</p>
-     <ul>
-      <li>Go to your backend and modify the config url</li>
-      <li>Make sure you clicked on the <b>use template</b> button on github</li>
-      <li>This template is using <a style={LinkWord} target="_blank" href="https://styled-components.com/">styled components</a>, you don't have to use it</li>
-      <li>You don't have to follow the folder structure, feel free to adapt to your own</li>
-      <li>Login and SignUp are already implemented</li>
-      <li>Modify this page to create your own homeepage</li>
-     </ul>
+      <h3>Hello there 👋</h3>
+      <p>General information:</p>
+
+      <button
+        onClick={(e) => {
+          e.preventDefault();
+          navigate("/puzzle1");
+        }}
+      >
+        Puzzle
+      </button>
     </Container>
-  )
-}
+  );
+};
 
 const Container = styled.div`
-  margin: 20px
-`
+  margin: 20px;
+`;
---
+++ src/pages/puzzles/PointToPointCursor.js
@@ -0,0 +1,113 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
+export const PointToPointCursor = () => {
+  const navigate = useNavigate();
+  const [failed, setFailed] = useState(false);
+  const [passed, setPassed] = useState(false);
+  let field, width, height, startX, startY;
+  document.addEventListener("mousemove", mousePos);
+
+  window.onload = function () {
+    field = document.getElementById("field");
+    startX = field.getBoundingClientRect().left;
+    startY = field.getBoundingClientRect().top;
+    width = parseInt(field.offsetWidth);
+    height = parseInt(field.offsetHeight);
+  };
+
+  function mousePos(event) {
+    if (
+      event.clientX < startX + 50 &&
+      event.clientY < startY + 50 &&
+      event.clientX > startX &&
+      event.clientY > startY
+    ) {
+      console.log("Start");
+    } else if (event.clientX > width - 50 && event.clientY > height - 50) {
+      document.removeEventListener("mousemove", mousePos);
+      console.log("END");
+      setPassed(true);
+    } else if (!passed) {
+      setFailed(true);
+    }
+  }
+
+  return (
+    <Whole>
+      <Restart
+        onClick={(e) => {
+          window.location.reload();
+        }}
+        style={{ zIndex: failed ? 10 : -10 }}
+      >
+        Restart
+      </Restart>
+      <Continue style={{ zIndex: passed ? 10 : -10 }}>
+        You passed the test, on to the next one!
+        <button
+          onClick={(e) => {
+            e.preventDefault();
+            navigate("/puzzle2");
+          }}
+        >
+          Next problem
+        </button>
+      </Continue>
+      <Start></Start>
+      <Finish></Finish>
+      <Field id="field" />
+    </Whole>
+  );
+};
+
+const Whole = styled.div`
+  display: flex;
+  height: 100vh;
+  width: 100hw;
+`;
+
+const Field = styled.div`
+  display: flex;
+  margin: auto;
+  height: 100%;
+  width: 100%;
+  background-color: black;
+`;
+
+const Start = styled.div`
+  position: absolute;
+  width: 50px;
+  height: 50px;
+  background-color: white;
+`;
+
+const Finish = styled.div`
+  position: absolute;
+  width: 50px;
+  height: 50px;
+  background-color: green;
+  bottom: 0;
+  right: 0;
+`;
+
+const Restart = styled.button`
+  position: absolute;
+  width: 50px;
+  height: 50px;
+  background-color: red;
+`;
+
+const Continue = styled.div`
+  flex-direction: column;
+  display: flex;
+  color: white;
+  left: 40%;
+  top: 40%;
+  margin: auto;
+  position: absolute;
+  background-color: blue;
+  height: 20%;
+  width: 20%;
+`;
