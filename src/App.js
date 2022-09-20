293d9ea2f02f2cd76a821a5e031475566d803ef6
Miriamvdb <miriamvdb@live.nl>
2022-09-20 13:54:17 +0200

    Guess the number game

--- src/App.js
+++ src/App.js
@@ -4,25 +4,25 @@ import { useDispatch } from "react-redux";
 import { getUserWithStoredToken } from "./store/user/thunks";
 import { Routes, Route } from "react-router-dom";
 import { Navigation, MessageBox } from "./components";
-import { Homepage, Login, SignUp } from "./pages"
-
+import { Homepage, Login, SignUp } from "./pages";
+import { GuessTheNumber } from "./pages/puzzles/GuessTheNumber";
 
 function App() {
-
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getUserWithStoredToken());
   }, [dispatch]);
-  
+
   return (
     <div>
-      <Navigation/>
-      <MessageBox/>
+      <Navigation />
+      <MessageBox />
       <Routes>
-        <Route path="/" element={<Homepage />}/>
+        <Route path="/" element={<Homepage />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/guessthenumber" element={<GuessTheNumber />} />
       </Routes>
     </div>
   );
